650d5abda141d00fd91ceaa529e9f81fb1583972
Abigail Rolling <arolling@gmail.com>
2016-02-19 08:32:01 -0800

    Add basic functionality for definition object class

--- src/main/java/Definition.java
+++ src/main/java/Definition.java
@@ -1,3 +1,11 @@
 public class Definition {
-  
+  private String mDefinition;
+
+  public Definition(String newDefinition) {
+    mDefinition = newDefinition;
+  }
+
+  public String getDefinition() {
+    return mDefinition;
+  }
 }
--- src/test/java/DefinitionTest.java
+++ src/test/java/DefinitionTest.java
@@ -7,6 +7,17 @@ public class DefinitionTest {
   // public ClearRule clearRule = new ClearRule();
 
   // UNIT TESTING
+  @Test
+  public void Definition_instantiatesCorrectly_true(){
+    Definition testDefinition = new Definition("a member of the household of a high official");
+    assertEquals(true, testDefinition instanceof Definition);
+  }
+
+  @Test
+  public void getDefinition_returnsSpecificDefinition_Familiar(){
+    Definition testDefinition = new Definition("a member of the household of a high official");
+    assertEquals("a member of the household of a high official", testDefinition.getDefinition());
+  }
 
 
 
